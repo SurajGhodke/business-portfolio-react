43dfacbe329497af1cd7173391d391fefc06273c
Suraj Ghodke <surajghodke0102@gmail.com>
2021-01-09 23:07:08 +0530

    adding animation to about page

--- src/App.js
+++ src/App.js
@@ -7,28 +7,32 @@ import Nav from "./components/Nav";
 import ContactUs from "./pages/ContactUs";
 import OurWork from "./pages/OurWork";
 //router
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch, useLocation } from "react-router-dom";
 import MovieDetail from "./pages/MovieDetail";
+import { AnimatePresence } from "framer-motion";
 
 function App() {
+  const location = useLocation();
   return (
     <div className="App">
       <GlobalStyle />
       <Nav />
-      <Switch>
-        <Route path="/" exact>
-          <AboutUs />
-        </Route>
-        <Route path="/work" exact>
-          <OurWork />
-        </Route>
-        <Route path="/work/:id">
-          <MovieDetail />
-        </Route>
-        <Route path="/contact">
-          <ContactUs />
-        </Route>
-      </Switch>
+      <AnimatePresence exitBeforeEnter>
+        <Switch location={location} key={location.pathname}>
+          <Route path="/" exact>
+            <AboutUs />
+          </Route>
+          <Route path="/work" exact>
+            <OurWork />
+          </Route>
+          <Route path="/work/:id">
+            <MovieDetail />
+          </Route>
+          <Route path="/contact">
+            <ContactUs />
+          </Route>
+        </Switch>
+      </AnimatePresence>
     </div>
   );
 }
--- src/pages/ContactUs.js
+++ src/pages/ContactUs.js
@@ -1,7 +1,19 @@
 import React from "react";
+//animations
+import { motion } from "framer-motion";
+import { pageAnimation } from "./../animation";
 
 const ContactUs = () => {
-  return <div>contact</div>;
+  return (
+    <motion.div
+      variants={pageAnimation}
+      initial="hidden"
+      animate="show"
+      exit="exit"
+    >
+      contact
+    </motion.div>
+  );
 };
 
 export default ContactUs;
