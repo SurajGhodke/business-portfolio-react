4cac42ad3e784662e236b132aec5361c82ecbd53
Suraj Ghodke <surajghodke0102@gmail.com>
2021-01-09 13:06:54 +0530

    created nav bar and added react-router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import React from "react";
+//global style
+import GlobalStyle from "./components/GlobalStyle";
 //import pages
 import AboutUs from "./pages/AboutUs";
+import Nav from "./components/Nav";
+import ContactUs from "./pages/ContactUs";
+import OurWork from "./pages/OurWork";
+//router
+import { Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <AboutUs />
+      <GlobalStyle />
+      <Nav />
+      <Switch>
+        <Route path="/" exact>
+          <AboutUs />
+        </Route>
+        <Route path="/work">
+          <OurWork />
+        </Route>
+        <Route path="/contact">
+          <ContactUs />
+        </Route>
+      </Switch>
     </div>
   );
 }
